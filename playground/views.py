bb2eac8708d9c246227d7405ee7e34e43f6f10c7
PaccyC <abayisengaaimepacifique@gmail.com>
2024-04-24 22:04:52 +0200

    added querysets

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product
 # Create your views here.
 
-def calculate():
-    x=1
-    y=2
-    return x
     
 def say_hello(request):
-    x= calculate()
+    query_set=Product.objects.all()
+    for product in query_set:
+        print(product)
+    
     return render(request,'hello.html',{'name':'Noel'})
