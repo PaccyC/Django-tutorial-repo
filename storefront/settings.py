be0dfe48060366891492625f39dc868fc1428647
PaccyC <abayisengaaimepacifique@gmail.com>
2024-04-24 20:53:13 +0200

    Started using mysql instead of sqlite

--- storefront/settings.py
+++ storefront/settings.py
@@ -78,8 +78,15 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
