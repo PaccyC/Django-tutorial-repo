9b2f7420e2c121b6f3db3e5ebdebf0933bc5ce92
PaccyC <abayisengaaimepacifique@gmail.com>
2024-04-27 22:00:40 +0200

    Added sorting

--- store/views.py
+++ store/views.py
@@ -4,7 +4,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter,OrderingFilter
 from rest_framework.views import APIView
 
 from rest_framework.viewsets import ModelViewSet
@@ -36,9 +36,10 @@ from django.db.models import Count
 class ProductViewSet(ModelViewSet):
         queryset=Product.objects.all()
         serializer_class=ProductSerializer
-        filter_backends=[DjangoFilterBackend,SearchFilter]
+        filter_backends=[DjangoFilterBackend,SearchFilter,OrderingFilter]
         filterset_class=ProductFilter
         search_fields=['title','description']
+        ordering_fields=['price','last_updated']
         
         # MANUAL FILTERING
         
