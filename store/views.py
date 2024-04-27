5e339f5124f29ac6591f43670d6cbad95c1e3c89
PaccyC <abayisengaaimepacifique@gmail.com>
2024-04-27 11:25:32 +0200

    added serialization of relationships

--- store/views.py
+++ store/views.py
@@ -1,13 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .models import Collection, Product
+from .serializers import ProductSerializer,CollectionSerializer
 # Create your views here.
 
 @api_view(["GET"])
 def product_list(request):
-    return Response("OK")
+    query_set=Product.objects.select_related("collection").all()
+    serializer=ProductSerializer(query_set,many=True,context={'request':request})
+    return Response(serializer.data)
 
 @api_view(["GET"])
 def product_detail(request,id):
-    return Response(id)
+    
+      product= get_object_or_404(Product,pk=id)
+      serilizer=ProductSerializer(product)
+      return Response(serilizer.data)
+  
+  
+@api_view(["GET"])
+def collection_detail(request,pk):
+      collection=get_object_or_404(Collection,pk=pk)
+      serializer=CollectionSerializer(collection)
+      return Response(serializer.data)
