a28df4788d2aefe6c6caef5857de40fd1c57be0b
PaccyC <abayisengaaimepacifique@gmail.com>
2024-05-01 09:21:33 +0200

    added likedItem modek

--- core/admin.py
+++ core/admin.py
@@ -2,9 +2,23 @@ from django.contrib import admin
 from tags.models import TaggedItem
 from store.admin import ProductAdmin
 from store.models import Product
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.contenttypes.admin import GenericTabularInline
+from .models import User
 # Register your models here.
 
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+      add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2","email","first_name","last_name"),
+            },
+        ),
+    )
+    
 
 class TagInline(GenericTabularInline):
     model=TaggedItem
