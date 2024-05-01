06dd1be018c423259147cc5c6924a8c66fe6f74f
PaccyC <abayisengaaimepacifique@gmail.com>
2024-05-01 10:19:08 +0200

    adding custom permissions to order model

--- store/models.py
+++ store/models.py
@@ -89,6 +89,11 @@ class Order(models.Model):
    payment_status= models.CharField(max_length=1,choices=PAYMENT_STATUS_CHOICES,default=PAYMENT_PENDING)
    customer=models.ForeignKey(Customer,on_delete=models.PROTECT)
    
+   class Meta:
+       permissions=[
+           ('cancel_order','Can Cance Order')
+       ]
+   
    
 
 class OrderItem(models.Model):
