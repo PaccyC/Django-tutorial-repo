d2bfd71933670917fe210071c2e87257272c6d4d
PaccyC <abayisengaaimepacifique@gmail.com>
2024-05-05 11:02:08 +0200

    added ProductImage model

--- storefront/urls.py
+++ storefront/urls.py
@@ -1,5 +1,6 @@
 
-
+from django.conf  import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
 
@@ -14,3 +15,5 @@ urlpatterns = [
     path("store/",include("store.urls")),
     path("playground/",include('playground.urls'))
 ]
+if settings.DEBUG :
+    urlpatterns +=static(settings.MEDIA_URL,document_root= settings.MEDIA_ROOT)
