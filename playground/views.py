8bda75b7ce7bfaab90d013a89862758cca98b8b8
PaccyC <abayisengaaimepacifique@gmail.com>
2024-05-06 11:20:31 +0200

    sending templated mails

--- playground/views.py
+++ playground/views.py
@@ -1,16 +1,30 @@
 from django.core.mail import send_mail,mail_admins,EmailMessage,BadHeaderError
 from rest_framework.response import Response
 from django.shortcuts import render
+from templated_mail.mail import BaseEmailMessage
 # Create your views here.
 
     
 def say_hello(request):
     try:
         # send_mail('subject','message','pacifique@gmail.com',['elite@gmail.com'])
+        
+        # USING mail_admins
         # mail_admins("subjct here","message here",html_message='message')
-        message=EmailMessage("subject here","Body of the message",'paccy@gmail.com',['elite@gmail.com'])
-        message.attach_file('playground/static/images/cat.jpg')
-        message.send()
+        
+        
+        # ATTACHING FILES
+        
+        # message=EmailMessage("subject here","Body of the message",'paccy@gmail.com',['elite@gmail.com'])
+        # message.attach_file('playground/static/images/cat.jpg')
+        # message.send()
+        
+        # Sending templated mail
+        message=BaseEmailMessage(
+            template_name='emails/hello.html',
+            context={'name':'Paccy'})
+        message.send(['elite@gmail'])
+        
     except BadHeaderError:
         pass
     
